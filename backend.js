bbfb2b8fee62d1ff111dafc27919ba0a5273fb94
Rodrigo Seizo Kawakami Sanchez <24.95012-2@maua.br>
2024-10-25 12:39:04 -0300

    aula 25/10

--- backend.js
+++ backend.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const e = require('express')
 const app = express()
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require('bcrypt')
 app.use(express.json())
 app.use(cors())
 
@@ -10,6 +11,14 @@ const Filme = mongoose.model("Filme", mongoose.Schema({
     titulo: {type: String},
     sinopse: {type: String}
 }))
+
+const usuarioSchema = mongoose.Schema({
+    login: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model ("Usuario", usuarioSchema)
 
 async function conectarAoMongo() {
     await mongoose.connect(`mongodb+srv://rodrigo:mongo1@rodrigosanchez.hdsdd.mongodb.net/?retryWrites=true&w=majority&appName=RodrigoSanchez`)
@@ -31,8 +40,24 @@ app.post ('/filmes', async (req, res) => {
     // //exibir a base atualizada
     const filme = new Filme({titulo: titulo, sinopse: sinopse})
     await filme.save()
-    const filmes = await Filme.find
+    const filmes = await Filme.find()
     res.json(filmes)
+})
+
+app.post ('/signup', async (req, res) => {
+    try{
+        const login = req.body.login
+        const password = req.body.password
+        const password_criptografada = await bcrypt.hash(password, 10)
+        const usuario = new Usuario ({login: login, password: password_criptografada})
+        const respMongo = await usuario.save()
+        console.log(respMongo)
+        res.end();
+    }
+    catch (e){
+        console.log(e)
+        res.status(409).end()
+    }
 })
 
 app.listen (3000, () => {
@@ -40,7 +65,7 @@ app.listen (3000, () => {
         conectarAoMongo()
         console.log("server up & running & connection ok")
     }
-    catch{
+    catch (e){
         console.log("erro de conexão", e)
     }
 })
